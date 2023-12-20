13a635f8318eeae8870fe3ec8d673d82dd0a61b6
Evelyn Raña <ev.r@live.com.ar>
2023-12-20 16:13:24 -0300

    correccion de imagenes

---
+++ js/productos.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Plan anual",
+      precio: 75000,
+      descripcion: " 3 clases semanales ",
+      img: "pilates_anual.jpg",
+    aclaracion:"(pago único equivalente a $6250 /mes.)",
+    },
+    { id: 2, 
+      nombre: "Plan semestral", 
+    precio: 42000, 
+    descripcion: " 3 clases semanales ",
+    img: "pilates_semestre.jpg",
+    aclaracion:"(pago único equivalente a $7000 /mes.)",
+  },
+  { id: 3, 
+    nombre: "Plan mensual", 
+    precio: 10000, 
+    descripcion: " 3 clases semanales ",
+    img: "pilates_mensual.jpg" ,
+    aclaracion:"(pago único equivalente a $10000 /mes.)",
+  },
+  
+  ];
+  
